061011b897381aacb860ee9df15ffd512f310617
Luis Segovia <luiissegovia@icloud.com>
2022-03-02 14:14:56 -0700

    i added labels and outlets and button

--- FinalGrade/ViewController.swift
+++ FinalGrade/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var secTextfield: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButton(_ sender: Any) {
+    
+    }
+ 
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var firstLabel: UILabel!
 }
 
